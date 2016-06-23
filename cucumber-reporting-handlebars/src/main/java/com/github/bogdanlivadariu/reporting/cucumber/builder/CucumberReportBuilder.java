4471bacd61b138476e7bfe9e0907c3ed133a036f
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2016-06-23 21:49:44 +0300

    added suport to diplay tags for scenarios

--- cucumber-reporting-handlebars/src/main/java/com/github/bogdanlivadariu/reporting/cucumber/builder/CucumberReportBuilder.java
+++ cucumber-reporting-handlebars/src/main/java/com/github/bogdanlivadariu/reporting/cucumber/builder/CucumberReportBuilder.java
@@ -6,7 +6,6 @@ import static com.github.bogdanlivadariu.reporting.cucumber.helpers.Constants.FE
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
