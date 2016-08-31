758ab93268649085384149854ec616547a364baa
Bogdan Livadariu <bogdan.livadariu@gmail.com>
2016-08-31 13:56:10 +0300

    fixed problems with status labels for test ng

--- testng-reporting-handlebars/src/main/java/com/github/bogdanlivadariu/reporting/testng/helpers/Constants.java
+++ testng-reporting-handlebars/src/main/java/com/github/bogdanlivadariu/reporting/testng/helpers/Constants.java
@@ -1,13 +1,16 @@
 package com.github.bogdanlivadariu.reporting.testng.helpers;
 
+import org.testng.reporters.XMLReporterConfig;
+
 public class Constants {
-    public static final String SKIPPED = "skipped";
+    public static final String SKIPPED = XMLReporterConfig.TEST_SKIPPED;
 
-    public static final String PASSED = "passed";
+    public static final String PASSED = XMLReporterConfig.TEST_PASSED;
 
-    public static final String FAILED = "failed";
+    public static final String FAILED = XMLReporterConfig.TEST_FAILED;
 
-    public static final String UNDEFINED = "undefined";
+    public static final String UNDEFINED = "UNDEFINED";
 
-    private Constants() {}
+    private Constants() {
+    }
 }
